dd542e979d707552ac931495629503dbc63b9542
xNicky <nraveszani@gmail.com>
2017-06-18 14:42:38 -0300

    Update similares.py

--- similares.py
+++ similares.py
@@ -1,20 +1,13 @@
 def similares(grafo,usuario,n):
     dic={}
-    for i in range (3):#yo digo q lo haga tres veces el random walks pero se puede cambiar al toque
-        walks=random_walks(grafo,usuario,100)#dps vemos de cambiar la cosa jejeje
-        walks.pop(usuario)
-        lista= sorted(walks.items(), key=lambda x: x[1]) 
-        x=len(lista)
-        cont=x-n-1
-        while cont<x:
-            if lista[cont][0] in dic:
-                dic[lista[cont][0]]+=lista[cont][1]   
-            else:
-                dic[lista[cont][0]]=lista[cont][1]   
-            cont+=1
-    result= sorted(dic.items(), key=lambda x: x[1])
-    a=len(result)
-    aux=a-1
-    while aux>a-n:
-        print("{}-".format(result[aux][0]))
-        aux-=1
+    for i in range(0, 2):
+        walks = random_walks(grafo,usuario,20)
+        lista = sorted(walks.items(), key=lambda x:x[1])
+        for usuario, apariciones in lista:
+            dic[usuario] = dic[usuario].get(usuario, 0) + apariciones
+    result = sorted(dic.items(), key=lambda x:x[1])
+    for y in range(len(dic), len(dic)-n, -1):
+        if y == len(dic) - n:
+            print("{}".format(result[y][0]))
+        else:
+            print("{},".format(result[y][0]))
