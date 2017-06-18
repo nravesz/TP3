8b514581bc6e7f50e40cdfdf1677552e95a587b8
xNicky <nraveszani@gmail.com>
2017-06-18 20:05:50 -0300

    Update random_walks.py

--- random_walks.py
+++ random_walks.py
@@ -1,11 +1,14 @@
 def random_walks(grafo,vertice,n):
-    actual=vertice
+    actual = vertice
     recorridos={}
-    for i in range (n):
-        adyacentes=grafo.obtener_adyacentes(actual)
+    for i in range(0, n):
+        adyacentes = grafo.obtener_adyacentes(actual)
         if not actual in recorridos:
-            recorridos[actual]=1
+            recorridos[actual] = 1
         else:
-            recorridos[actual]+=1
-        actual=random.choice(list(adyacentes))
+            recorridos[actual] += 1
+        if len(adyacentes) > 0:
+            actual = random.choice(list(adyacentes))
+        else:
+            break
     return recorridos
