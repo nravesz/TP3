fe6cef73f46656b904e16472914209f8f04f0b9e
xNicky <nraveszani@gmail.com>
2017-06-22 23:21:44 -0300

    Update comunidades.py

--- comunidades.py
+++ comunidades.py
@@ -1,31 +1,38 @@
 def comunidades(grafo):
-    contador_label = label_propagation(grafo)
+    contador_label, label_usuarios = label_propagation(grafo)
     ordenado = sorted(contador_label.items(), key=lambda x:x[1])
-    for i in range(0, len(ordenado) - 1, -1):
+    for i in range(len(ordenado) - 1, -1, -1):
         if ordenado[i][0] > 20000:
             continue
         elif ordenado[i][0] < 4:
             break
         else:
-            print("Comunidad: {}".format(ordenado[i][0]))
-            print("Cantidad de integrantes: {}".format(len(ordenado[i]) - 1))
+            comunidad = ordenado[i][0]
+            print("Comunidad: {}".format(comunidad))
+            lista_usuarios = label_usuarios[comunidad]
+            print("Cantidad de integrantes: {}".format(len(lista_usuarios)))
             print("Integrantes:")
-            for j in range(1, len(ordenado[i]) - 1, 1):
-                print("{}: {}".format(j, ordenado[j]))
+            print(lista_usuarios)
     return
 
 def label_propagation(grafo):
     label, contador_label = asignar_etiquetas(grafo)
     ids = grafo.obtener_identificadores()
     actual = random.choice(list(ids))
-    n = int(vertices/4) # Elegido de forma arbitraria
+    n = int(grafo.cantidad_vertices()/4) # Elegido de forma arbitraria
     for i in range(0, n, 1):
-        apariciones = aproximacion_de_apariciones(grafo, actual, 1, 1) #si recibe 1 y 1, solo itera entre los adyacentes y nada mas
-        apariciones = sorted(apariciones.items(), key=lambda x:x[1])
-        label[actual] = apariciones[0][1]
-        contador_label[apariciones[0][1]] = dic.get(apariciones, 0) + 1
+        if len(grafo.obtener_adyacentes(actual)) > 0:
+            apariciones = aproximacion_de_apariciones(grafo, actual, 15, 1) #si recibe 1 y 1, solo itera entre los adyacentes y nada mas
+            apariciones = sorted(apariciones.items(), key=lambda x:x[1])
+            if len(apariciones) > 0:
+                usuario, cantidad = apariciones[0]
+                label[actual] = label[usuario]
+                contador_label[label[usuario]] = contador_label.get(label[usuario], 0) + 1
         actual = random.choice(list(ids))
-    return contador_label
+    label_usuarios = {}
+    for usuario, etiqueta in label.items():
+        label_usuarios[etiqueta] = label_usuarios.get(etiqueta,[]) + [usuario]
+    return contador_label, label_usuarios
 
 def asignar_etiquetas(grafo):
     label = {}
@@ -34,6 +41,6 @@ def asignar_etiquetas(grafo):
     etiqueta = 0 #0 de originalidad (no puedo poner tantas casas de GOT)
     for vertice in vertices:
         label[vertice] = etiqueta
-        contador_label[etiqueta] = dic[etiqueta].get(etiqueta, 0) + 1
+        contador_label[etiqueta] = contador_label.get(etiqueta, 0) + 1
         etiqueta += 1
     return label, contador_label
